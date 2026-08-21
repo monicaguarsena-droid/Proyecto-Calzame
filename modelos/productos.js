cf7bc31f9ac89f738f750088351bdb44c8e2947a
monicaguarsena <monicaguarsena@gmail.com>
2026-08-21 11:28:44 -0500

    realizar cambios en productos

--- modelos/productos.js
+++ modelos/productos.js
@@ -7,12 +7,25 @@ export const obtenerProductos = async () => {
         .select('*');
     return { data, error };
 };
-
+export const obtenerPorCategoria = async (categoria) => {
+    const { data, error } = await supabase.from('productos').select('*').eq('categoria', categoria);
+    return { data, error };
+};
 // coloca un zapato nuevo en la tabla productos
-export const crearProducto = async (nombre, descripcion, precio, talla, stock, imagen) => {
+export const crearProducto = async (productoData) => {
     const { data, error } = await supabase
         .from('productos')
-        .insert([{ nombre, descripcion, precio, talla, stock, imagen }])
+        .insert(productoData)
         .select();
  return { data, error };
+};
+export const actualizarproducto = async (id, productoData) => {
+    const { data, error } = await supabase.from('productos').update(productoData).eq('id', id).select();
+    return { data, error };
+};
+
+
+export const eliminarProducto = async (id) => {
+    const { data, error } = await supabase.from('productos').delete().eq('id', id).select();
+    return { data, error };
 };
