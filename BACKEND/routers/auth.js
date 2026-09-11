211d8aeb917d67d7216a51dad94f36c1772249f2
monicaguarsena <monicaguarsena@gmail.com>
2026-09-11 07:46:06 -0500

    agregar ruta de verificar codigo y cambios en comentarios

--- BACKEND/routers/auth.js
+++ BACKEND/routers/auth.js
@@ -1,4 +1,4 @@
-import { registro, login } from '../controllers/auth.js';
+import { registro, login,verificarCuenta } from '../controllers/auth.js';
 import { forgotPassword,verifyCode } from '../controllers/recuperar.js';
 import { Router } from 'express';
 import { autenticarConGoogle } from '../controllers/googleauth.js';
@@ -8,12 +8,13 @@ const router = Router();
 //rutas de autenticacion
 router.post('/register', registro);
 router.post('/login', login);
+router.post('/verify-account',verificarCuenta);
 
-//ruta de olvido contrasaeña
+//ruta de olvido contraseña
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-code', verifyCode);
-//ruta la auntentificar con google
 
+//ruta la auntentificar con google
 router.post('/google', autenticarConGoogle);
 
 export default router;
